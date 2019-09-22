10cbfb713501a26a1c097f045087d9cb94f1911a
djchicken <djchicken@pop-os.localdomain>
2019-09-22 11:16:15 -0400

    Added feature to update

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 import smtplib
 import requests
+import os
+
+no = ['no','No','NO','N','n']
+yes = ['yes','Yes','YES','YEs','YeS','yES','y','Y']
 
 class bcolors:
-    GREEN = '\033[92m'
-    YELLOW = '\022[93m'
-    RED = '\033[91m'
+    PROMPT = '\033[33m'
+    ERROR = '\033[91m'
+    OK = '\033[92m'
+
+def start():
+    print("start")
 
 def checkVersion():
     versionFile = open("version.txt")
@@ -16,35 +23,55 @@ def checkVersion():
 
 def checkUpdate():
     githubVersion = requests.get('https://raw.githubusercontent.com/TheMasterOfE/PhishBox/master/version.txt')
-    latestVersion = githubVersion.text
-    float(latestVersion)
-    if latestVersion == version:
-        print(bcolors.GREEN + 'Looks like PhishBox is up to date!')
+    statusCode = githubVersion.status_code
+    int(statusCode)
+    if statusCode == 200:
+        latestVersion = githubVersion.text
+        float(latestVersion)
+        if latestVersion == version:
+            print(bcolors.OK + 'Looks like PhishBox is up to date!')
+        else:
+            print(bcolors.ERROR + 'It looks like you are using an outdated version of PhishBox!')
+            promptUpdate()
     else:
-        print(bcolors.RED + 'It looks like you are using an outdated version of PhishBox!')
+        print(bcolors.ERROR + "ERROR " + statusCode)
 
+def update():
+    print(bcolors.OK + "E")
+    os.system("cd ..")
+    os.system("rm -rf PhishBox")
+    os.system("git clone https://github.com/TheMasterOfE/PhishBox")
+
+def promptUpdate():
+    willUpdate = input(bcolors.PROMPT + "Would you like to update Y/N\n")
+    if willUpdate in yes:
+        update()
+    elif willUpdate in no:
+        start()
+    else:
+        print("Please enter either Yes or No")
+        promptUpdate()
 
 def banner():
-    print(bcolors.GREEN + "██████╗ ██╗  ██╗██╗███████╗██╗  ██╗██████╗  ██████╗ ██╗  ██╗")
-    print(bcolors.GREEN + "██╔══██╗██║  ██║██║██╔════╝██║  ██║██╔══██╗██╔═══██╗╚██╗██╔╝")
-    print(bcolors.GREEN + "██████╔╝███████║██║███████╗███████║██████╔╝██║   ██║ ╚███╔╝")
-    print(bcolors.GREEN + "██╔═══╝ ██╔══██║██║╚════██║██╔══██║██╔══██╗██║   ██║ ██╔██╗")
-    print(bcolors.GREEN + "██║     ██║  ██║██║███████║██║  ██║██████╔╝╚██████╔╝██╔╝ ██")
-    print(bcolors.GREEN + "╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝")
+    print(bcolors.OK + "██████╗ ██╗  ██╗██╗███████╗██╗  ██╗██████╗  ██████╗ ██╗  ██╗")
+    print(bcolors.OK + "██╔══██╗██║  ██║██║██╔════╝██║  ██║██╔══██╗██╔═══██╗╚██╗██╔╝")
+    print(bcolors.OK + "██████╔╝███████║██║███████╗███████║██████╔╝██║   ██║ ╚███╔╝")
+    print(bcolors.OK + "██╔═══╝ ██╔══██║██║╚════██║██╔══██║██╔══██╗██║   ██║ ██╔██╗")
+    print(bcolors.OK + "██║     ██║  ██║██║███████║██║  ██║██████╔╝╚██████╔╝██╔╝ ██")
+    print(bcolors.OK + "╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝")
 
     # Please change the version and edit date when changes are made cause it looks offical :)
     # A note to future us we could impliment an api to check the latest commit and version to see if an update is available
-    print(bcolors.GREEN + "Version " + version)
-    print(bcolors.GREEN + "Last edit: 9/20/19")
-    print(bcolors.GREEN + "By DJCHICKEN4, Commi3, ???")
-    print(bcolors.GREEN + "This is to be used for legal and educational purposes only. Any other use is not intended.")
+    print(bcolors.OK + "Version " + version)
+    print(bcolors.OK + "Last edit: 9/20/19")
+    print(bcolors.OK + "By DJCHICKEN4, Commi3, ???")
+    print(bcolors.OK + "This is to be used for legal and educational purposes only. Any other use is not intended.")
 
 class phishBox:
     count = 0
 
     def __init__(self):
             print("\n+{+{+{Initializing PhishBox}+}+}+")
-
 
 checkVersion()
 banner()
